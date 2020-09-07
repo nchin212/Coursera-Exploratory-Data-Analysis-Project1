911ab4f10edd5951d89e0de4923c0a68a2cdf2b5
nchin212 <nchin212@gmail.com>
2020-09-07 22:22:56 +0800

    Changed plot4

--- Completed Tasks/plot4.R
+++ Completed Tasks/plot4.R
@@ -28,7 +28,7 @@ plot(data_s$DateTime, data_s$Voltage, type ='l', xlab ="datetime", ylab="Voltage
 plot(data_s$DateTime, data_s$Sub_metering_1, col='black', type='l', xlab="", ylab="Energy sub metering")
 lines(data_s$DateTime, data_s$Sub_metering_2, col='red')
 lines(data_s$DateTime, data_s$Sub_metering_3, col='blue')
-legend("topright" ,legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col=c("black","red","blue"), lty=c(1,1,1))
+legend("topright" ,legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col=c("black","red","blue"), lty=c(1,1,1), bty='n')
 
 plot(data_s$DateTime, data_s$Global_reactive_power, type='l', xlab ="datetime", ylab="Global_reactive_power")
 dev.off()
